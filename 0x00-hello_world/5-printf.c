ce13048a86b447bfa4a598c45e73ed0b686e971a
nikikiarie <nix.kiariebiz@gmail.com>
2023-10-05 16:15:31 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entrance function
+ *
+ * Return: returns 0 when complete
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
