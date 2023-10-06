1fbb6a424cd6b54be155bb5a88f963520cfa9389
nikikiarie <nix.kiariebiz@gmail.com>
2023-10-06 07:57:56 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entrance function
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n')
+	return (0);
+}
