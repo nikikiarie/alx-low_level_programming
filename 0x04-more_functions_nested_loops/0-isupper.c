34d66ef119ddd770aa26a64841352e29bf7773e3
nikikiarie <nix.kiariebiz@gmail.com>
2023-10-12 07:02:29 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: checked char
+ * Return: 1 if uppercase 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isupper(int c);
+int mul(int a, int b);
+int _isdigit(int c);
+
+#endif
