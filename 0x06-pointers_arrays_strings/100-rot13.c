599d84f91d03a430b7da0d1fbbb35687fb9b2f57
nikikiarie <nix.kiariebiz@gmail.com>
2023-10-19 10:29:10 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,7 +9,7 @@
 char *rot13(char *c)
 {
 	int i, j;
-	char d1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYX";
+	char d1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char dr[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; c[i] != '\0' ; i++)
