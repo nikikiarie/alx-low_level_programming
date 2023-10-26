0386f1a64985f1ef869c93b0b584f5a30d777e3e
nikikiarie <nix.kiariebiz@gmail.com>
2023-10-25 17:13:28 -0700

    Once an idea has taken hold of the brain it's almost impossible to eradicate

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+int sqt(int n, int i);
+
+int _sqrt_recursion(int n)
+{
+	return (sqt(n, 1));
+}
+/**
+ * _sqt - natural square root of a number
+ * @n: num
+ * @i: square root
+ * Return: int
+ */
+int sqt(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if (i * i < n)
+		return (sqt(n, i + 1));
+	else
+		return (-1);
+}
