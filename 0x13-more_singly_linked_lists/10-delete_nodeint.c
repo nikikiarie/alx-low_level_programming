abc15e0c493946b53c3792d3e8b9523f0874aacf
nikikiarie <nix.kiariebiz@gmail.com>
2023-11-21 12:41:38 -0800

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(t);
 		return (1);
 	}
-	while (i < index -1)
+	while (i < index - 1)
 	{
 		if (!t || !(t->next))
 			return (-1);
