7b2778ed32341e072bf9f4830709f0bc79d5698c
nikikiarie <nix.kiariebiz@gmail.com>
2024-03-20 10:16:22 -0700

    100 makefile

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Island perimeter function"""
+
+
+def island_perimeter(grid):
+    """Returns island perimeter
+
+
+    Args:
+        grid(list): list of int
+    Returns:
+        island perimeter
+    """
+    line = 0
+    h = len(grid)
+    w = len(grid[0])
+    size = 0
+
+    for i in range(h):
+        for k in range(w):
+            if grid[i][k] == 1:
+                size += 1
+                if (k > 0 and grid[i][k -1] == 1):
+                    line += 1
+                if (i > 0 and grid[i - 1][k] == 1):
+                    line += 1
+    return size * 4 - line * 2
